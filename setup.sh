88af45b1ae62d45417623da4d15d48df2dff23d7
bilogic <946010+bilogic@users.noreply.github.com>
2023-11-01 14:14:49 +0800

    sudo postmap

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 php -r "require_once 'MessageHelper.php'; (new MessageHelper())->setup();"
 
 sudo cp transport_maps /etc/postfix/transport_maps
-postmap /etc/postfix/transport_maps
+sudo postmap /etc/postfix/transport_maps
 
 sudo chown www-data:www-data mail -R
 sudo chmod +x postfix-filter.php
