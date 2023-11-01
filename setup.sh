d17ce3352bd7b782bf915345fd785b85a9b7c647
bilogic <946010+bilogic@users.noreply.github.com>
2023-11-01 16:28:00 +0800

    added missing commands

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-php -r "require_once 'MessageHelper.php'; (new MessageHelper())->setup();"
+sudo php -r "require_once 'MessageHelper.php'; (new MessageHelper())->setup();"
 
 sudo cp transport_maps /etc/postfix/transport_maps
 sudo postmap /etc/postfix/transport_maps
 
+sudo chown www-data:www-data default -R
 sudo chown www-data:www-data mail -R
 sudo chmod +x postfix-filter.php
+
+sudo ~/mailinabox/tools/web_update
+sudo service nginx restart
