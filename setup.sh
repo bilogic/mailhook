b2af67d30d440d8ac3d1dd3015f05f6098de8c69
bilogic <946010+bilogic@users.noreply.github.com>
2023-11-02 08:43:24 +0800

    refactor

--- setup.sh
+++ setup.sh
@@ -2,21 +2,25 @@
 
 sudo php -r "require_once 'PostfixFilter.php'; (new PostfixFilter())->setup('mail')->setup('bounce');"
 
-sudo cp transport_maps /etc/postfix/transport_maps
-sudo postmap /etc/postfix/transport_maps
-
-sudo chown www-data:www-data default -R
-
+# setup postfix filters
 sudo chmod 0700 pf-forwardmail.php
 sudo chown www-data:www-data pf-forwardmail.php
 
 sudo chmod 0700 pf-bulkbounce.php
 sudo chown www-data:www-data pf-bulkbounce.php
 
+# restart postfix
+sudo cp transport_maps /etc/postfix/transport_maps
+sudo postmap /etc/postfix/transport_maps
+sudo service postfix restart
+
+# make sure http is ok
+echo "# Listing domain conf file(s). Must have at least 1"
+ls *.conf
+
+sudo chown www-data:www-data default -R
 sudo ~/mailinabox/tools/web_update
 sudo service nginx restart
 
-echo "# Listing domain conf file(s). Must have at least 1"
-ls *.conf
 echo
 echo "# Done"
