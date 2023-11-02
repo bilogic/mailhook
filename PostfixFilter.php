7b3a2aba04871ba6a72edf4f873ff4799ed13952
bilogic <946010+bilogic@users.noreply.github.com>
2023-11-02 08:36:56 +0800

    fixed crlf

--- PostfixFilter.php
+++ PostfixFilter.php
@@ -241,8 +241,8 @@ class PostfixFilter
             $transport_maps .= "/^$email/ forwardmail:dummy\r\n";
         }
 
-        $transport_maps .= '/^bounce@e115.com/   bulkbounce:';
-        $transport_maps .= '/.*/ :';
+        $transport_maps .= "/^bounce@e115.com/   bulkbounce:\r\n";
+        $transport_maps .= "/.*/ :\r\n";
 
         return $transport_maps;
     }
