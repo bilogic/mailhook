b340353e14b4bf5fd39381fea7106330a72e0c51
bilogic <946010+bilogic@users.noreply.github.com>
2023-11-03 21:34:46 +0800

    fix

--- PostfixFilter.php
+++ PostfixFilter.php
@@ -254,13 +254,14 @@ class PostfixFilter
         echo "- Notifying [$url]\r\n";
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
-        curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+        //curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         $output = curl_exec($ch);
         $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
+        echo "$output\r\n";
         echo "- HTTP response code [$httpcode]\r\n";
 
         if ($httpcode == 200) {
