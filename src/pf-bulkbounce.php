8c0afbffdee52f295e931a674e9b64ab18fe465a
bilogic <946010+bilogic@users.noreply.github.com>
2023-11-25 16:45:04 +0800

    add user log when running

--- src/pf-bulkbounce.php
+++ src/pf-bulkbounce.php
@@ -8,6 +8,8 @@
 
 require_once 'PostfixFilter.php';
 
+syslog(LOG_INFO, '[pf-bulkbounce.php] running as '.get_current_user());
+
 $m = new PostfixFilter;
 $m->folder('../mail-bounced')
     ->save();
