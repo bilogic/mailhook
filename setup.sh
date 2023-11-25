714ef09e773d45c908cb05eef197aa98dbdd87e4
bilogic <946010+bilogic@users.noreply.github.com>
2023-11-25 10:42:57 +0800

    fix

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ sudo chmod 0770 mail-forward -R
 sudo adduser www-data user-data
 sudo chown www-data:www-data default/*php -R
 sudo chown www-data:www-data src/*php -R
+sudo chmod 0750 default
 sudo chmod 0700 src/pf-forwardmail.php
 sudo chmod 0700 src/pf-bulkbounce.php
 
