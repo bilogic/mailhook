2676e54e18db1801bd8837f75605c42210cebb8b
bilogic <946010+bilogic@users.noreply.github.com>
2023-11-26 12:13:36 +0800

    refactor to test all headers

--- tests/Feature/DSNTest.php
+++ tests/Feature/DSNTest.php
@@ -7,18 +7,29 @@ use PHPUnit\Framework\TestCase;
 
 class DSNTest extends TestCase
 {
-    protected $activity = null;
-
     public function test_can_parse_postfix_dsn()
     {
         $parser = (new Dsn)->parse(__DIR__.'/postfix-dsn.txt');
-        $pair = $parser->getAllHeaders();
+        $headers = $parser->getAllHeaders();
 
-        $this->assertEquals('1234567890', $pair['Mailhook-Id']);
-        $this->assertEquals('box@senderdomain.com', $pair['Return-Path']);
+        $expected['Return-Path'] = 'box@senderdomain.com';
+        $expected['DKIM-Signature'] = '1';
+        $expected['Received'] = "by box.smtpdomain.com (Postfix, from userid 1000)\r\n        id A492D3F147; Sat, 25 Nov 2023 19:51:36 +0800 (+08)";
+        $expected['To'] = 'nonexistent@example.com';
+        $expected['Mailhook-Id'] = '1234567890';
+        $expected['Subject'] = 'Hey, I successfully configured Postfix with sender-dependent SASL authentication!';
+        $expected['Content-type'] = 'text/html';
+        $expected['Message-Id'] = '20231125115136.A492D3F147@box.smtpdomain.com';
+        $expected['Date'] = 'Sat, 25 Nov 2023 19:51:36 +0800';
+        $expected['From'] = 'box@senderdomain.com';
+        $expected['Final-Recipient'] = 'rfc822; nonexistent@example.com';
+        $expected['Original-Recipient'] = 'rfc822;nonexistent@example.com';
+        $expected['Action'] = 'failed';
+        $expected['Status'] = '5.1.0';
+        $expected['Diagnostic-Code'] = 'X-Postfix; Domain example.com does not accept mail';
 
-        $this->assertEquals('rfc822;nonexistent@example.com', $pair['Original-Recipient']);
-        $this->assertEquals('5.1.0', $pair['Status']);
-        $this->assertEquals('X-Postfix; Domain example.com does not accept mail', $pair['Diagnostic-Code']);
+        foreach ($expected as $key => $value) {
+            $this->assertEquals($value, $headers[$key]);
+        }
     }
 }
