dddb0e3e2ea6584aa6fdeaaf9c81a297ca2c8df9
bilogic <946010+bilogic@users.noreply.github.com>
2023-11-26 17:00:23 +0800

    namespace fixes

--- remove.sh
+++ remove.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo php -r "require_once 'PostfixFilter.php'; (new PostfixFilter())->folder('mail')->remove();"
+sudo php -r "require_once 'src/PostfixFilter.php'; (new App\PostfixFilter())->folder('mail-forward')->remove();"
