e947632663acc41d52316a24822745ca465a832e
bilogic <946010+bilogic@users.noreply.github.com>
2023-11-26 19:17:43 +0800

    call closure even if no config

--- src/PostfixFilter.php
+++ src/PostfixFilter.php
@@ -117,22 +117,24 @@ class PostfixFilter
 
                         if (! isset($config[$dst])) {
                             $this->log("- Cannot find config for $dst");
-                        } else {
-                            $result = ($closure)($this, $config, $tell, $mailfile);
-
-                            // $url = $config[$dst].urlencode($filename);
-                            // $message = "Mail for $dst, piping to: {$url}";
-                            // $this->log( "$message");
-                            // file_put_contents('/var/log/pipe.log', $message, FILE_APPEND);
-
-                            // if ($this->isNotifyUrlSuccess($url)) {
-                            if ($result) {
-                                $this->log('- Handler was successful');
-                                @unlink($tellfile);
-                            } else {
-                                $this->log('- Handler failed');
-                            }
+                            $config = [];
                         }
+
+                        $result = ($closure)($this, $config, $tell, $mailfile);
+
+                        // $url = $config[$dst].urlencode($filename);
+                        // $message = "Mail for $dst, piping to: {$url}";
+                        // $this->log( "$message");
+                        // file_put_contents('/var/log/pipe.log', $message, FILE_APPEND);
+
+                        // if ($this->isNotifyUrlSuccess($url)) {
+                        if ($result) {
+                            $this->log('- Handler was successful');
+                            @unlink($tellfile);
+                        } else {
+                            $this->log('- Handler failed');
+                        }
+
                     }
                 }
                 $mutex->unlock();
